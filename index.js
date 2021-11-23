08827a4737cb9471bbd70d39c3b4fc8c6a37c49f
m-saqib-dev <m.saqibsiddiqui.dev@gmail.com>
2021-11-22 22:49:58 -0800

    new style

--- index.js
+++ index.js
@@ -1,23 +1,50 @@
-const auth = document.getElementById("auth");
-const loginBtn = document.getElementById("login-btn");
-const registerBtn = document.getElementById("register-btn");
+const openMenu = document.getElementById("open-menu");
+const closeMenu = document.getElementById("close-menu");
+const drawerMenu = document.getElementById("drawer-menu");
+const timer = document.getElementById("timer");
+const togglebtn = document.getElementsByClassName("toggle-btn");
+console.log();
+let isOpen = true;
+function menu() {
+  console.log(isOpen);
+  if (isOpen) {
+    isOpen = false;
+    togglebtn[0].children[0].style.display = "none";
+    togglebtn[0].children[1].style.display = "block";
+    drawerMenu.style.left = "0%";
+  } else {
+    isOpen = true;
+    drawerMenu.style.left = "100%";
+    togglebtn[0].children[1].style.display = "none";
+    togglebtn[0].children[0].style.display = "block";
+  }
+}
+togglebtn[0].addEventListener("click", () => {
+  menu();
+});
+const deadline = "December 31 2021 23:59:59 GMT+0200";
+function getTimeRemaining(endtime) {
+  const total = Date.parse(endtime) - Date.parse(new Date());
+  const millieseconds = Math.floor((total * 5) % 60);
+  const seconds = Math.floor((total / 1000) % 60);
+  const minutes = Math.floor((total / 1000 / 60) % 60);
+  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+  const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-window.addEventListener("scroll", (e) => {
-  let b = () => {
-    document.getElementById("social").style.top = `${window.scrollY + 100}px`;
-    document.getElementById("auth").style.top = `${window.scrollY + 40}px`;
-    requestAnimationFrame(b);
+  return {
+    total,
+    days,
+    hours,
+    minutes,
+    seconds,
+    millieseconds,
   };
-  b();
-});
-
-loginBtn.addEventListener("mousedown", () => {
-  document.getElementById("login-form").style.display = "grid";
-  loginBtn.style.display = "none";
-  registerBtn.style.display = "none";
-});
-registerBtn.addEventListener("mousedown", () => {
-  document.getElementById("register-form").style.display = "grid";
-  loginBtn.style.display = "none";
-  registerBtn.style.display = "none";
-});
+}
+const anim = () => {
+  let time = getTimeRemaining(deadline);
+  timer.innerHTML = `days ${time.days} hours ${time.hours} minutes ${time.minutes} seconds ${time.seconds} m seconds ${time.millieseconds}`;
+  requestAnimationFrame(anim);
+};
+anim();
+requestAnimationFrame(getTimeRemaining);
+console.log(getTimeRemaining(deadline));
