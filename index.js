d6badc7ba926797222ac2080ac9a017136338d9b
m-saqib-dev <m.saqibsiddiqui.dev@gmail.com>
2021-11-22 22:55:01 -0800

    new

--- index.js
+++ index.js
@@ -22,29 +22,3 @@ function menu() {
 togglebtn[0].addEventListener("click", () => {
   menu();
 });
-const deadline = "December 31 2021 23:59:59 GMT+0200";
-function getTimeRemaining(endtime) {
-  const total = Date.parse(endtime) - Date.parse(new Date());
-  const millieseconds = Math.floor((total * 5) % 60);
-  const seconds = Math.floor((total / 1000) % 60);
-  const minutes = Math.floor((total / 1000 / 60) % 60);
-  const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-  const days = Math.floor(total / (1000 * 60 * 60 * 24));
-
-  return {
-    total,
-    days,
-    hours,
-    minutes,
-    seconds,
-    millieseconds,
-  };
-}
-const anim = () => {
-  let time = getTimeRemaining(deadline);
-  timer.innerHTML = `days ${time.days} hours ${time.hours} minutes ${time.minutes} seconds ${time.seconds} m seconds ${time.millieseconds}`;
-  requestAnimationFrame(anim);
-};
-anim();
-requestAnimationFrame(getTimeRemaining);
-console.log(getTimeRemaining(deadline));
